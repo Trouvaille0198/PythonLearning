0c7f175263348ca4de2f9558dd5b0189d2103217
Trouvaille0198 <1375911011@qq.com>
2020-12-23 17:14:33 +0800

    芜湖

--- CrawlerPractices/RequestsTest/demo21.py
+++ CrawlerPractices/RequestsTest/demo21.py
@@ -4,7 +4,7 @@ url = 'http://httpbin.org/post'
 data = {'name': 'germey'}
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
-    }
+}
 s = Session()
 req = Request('POST', url, data=data, headers=headers)
 prepped = s.prepare_request(req)
--- MachineLearning/hw1.py
+++ MachineLearning/hw1.py
@@ -1,4 +1,34 @@
+from bs4 import BeautifulSoup
+import pandas as pd
 import requests
-payload = {'key1': 'value1', 'key2': 'value2'}
-r = requests.get("http://httpbin.org/get", params=payload)
-print(r.text)
+复制代码
+lst = []
+url = 'https://tophub.today/n/mproPpoq6O'
+
+
+def get(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36'}
+        url = requests.get(url, timeout=30, headers=headers)
+        url.raise_for_status()
+        url.encoding = 'utf-8'
+        return url.text
+    except:
+        return "产生异常"
+
+
+def create(lst, html, num):
+        soup = BeautifulSoup(html, 'html.parser'
+                             b=soup.find_all('span', class_='e')
+                             print('{:^10}\t{:^30}\t{:^10}'.format(
+                                 '排名', '标题', '热度'))
+                             for i in range(num):
+                             print('{:^10}\t{:^30}\t{:^10}'.format(i+1, a[i+50].string, b[i+50].string)
+                                   lst.append([i+1, a[i+50].string, b[i+50].string]
+                                              html=get(url)
+                                              create(lst, html, 10)
+                                              df=pd.DataFrame(
+                                                  lst, columns=['排名', '标题', '热度'])
+                                              ZHHot='D:\Python\知乎热搜'
+                                              df.to_excel(ZHHot)
