543909c73a4e719862838ed7a5130f42e2ca22f9
Trouvaille0198 <1375911011@qq.com>
2021-02-15 13:07:06 +0800

    2021/2/15 13:08 push

--- Test.py
+++ Test.py
@@ -1,1 +1,14 @@
-print("???")
+from manim import *
+
+
+class test(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+        circle.set_fill(PINK, opacity=0.5)
+
+        self.play(ShowCreation(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
